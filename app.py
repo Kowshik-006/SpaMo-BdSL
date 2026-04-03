1a56d9ee1c2b65f17fc61718eb09ecfeec5b1b60
Kowshik Saha <kowshik.saha.ks.ks@gmail.com>
2026-04-03 18:32:25 +0600

    Moved app.py out of the web-ui directory.

--- app.py
+++ app.py
@@ -6,12 +6,7 @@ import tempfile
 import gradio as gr
 from PIL import Image
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-
-# Add the parent directory to sys.path
-if parent_dir not in sys.path:
-    sys.path.append(parent_dir)
+sys.path.append(os.path.dirname(__file__))
 
 from omegaconf import OmegaConf
 from transformers import AutoImageProcessor, CLIPVisionModel
