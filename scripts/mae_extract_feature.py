c2a8f576deb13c93803b6ef421290c82abdef5a7
Kowshik Saha <kowshik.saha.ks.ks@gmail.com>
2026-07-09 14:08:19 +0600

    Modified vit mae code to work on kaggle

--- scripts/mae_extract_feature.py
+++ scripts/mae_extract_feature.py
@@ -1,11 +1,11 @@
 import os
 import numpy as np
 import torch
-import argparse
 import tqdm
 import os.path as osp
 from PIL import Image
 from transformers import VideoMAEModel, VideoMAEImageProcessor
+from transformers.utils import logging as hf_logging
 
 import sys
 sys.path.append('./')
@@ -20,58 +20,89 @@ from utils.feature_extract_resume import (
     handle_keyboard_interrupt,
 )
 
+# Decoder weights in the VideoMAE checkpoint are unused by VideoMAEModel (encoder-only).
+hf_logging.set_verbosity_error()
+os.environ.setdefault('TRANSFORMERS_NO_ADVISORY_WARNINGS', '1')
+
 _GLOBAL_SEED = 0
 np.random.seed(_GLOBAL_SEED)
 torch.manual_seed(_GLOBAL_SEED)
 torch.backends.cudnn.benchmark = True
 
 
+def configure_gpu(device='cuda:0'):
+    if not torch.cuda.is_available():
+        print('CUDA not available; falling back to CPU.')
+        return 'cpu', False
+
+    device_id = int(device.split(':')[-1]) if ':' in device else 0
+    name = torch.cuda.get_device_name(device_id)
+    major, minor = torch.cuda.get_device_capability(device_id)
+    use_fp16 = True
+    print(f'Using {name} on {device} (compute capability {major}.{minor}), fp16={use_fp16}')
+    return device, use_fp16
+
+
+def _to_device(inputs, device, use_fp16):
+    inputs = inputs.to(device)
+    if not use_fp16:
+        return inputs
+    return {
+        k: v.to(dtype=torch.float16) if v.is_floating_point() else v
+        for k, v in inputs.items()
+    }
+
+
 class VideoMAEFeatureReader(object):
     def __init__(
-        self, 
-        model_name='MCG-NJU/videomae-large', 
+        self,
+        model_name='MCG-NJU/videomae-large',
         cache_dir=None,
         device='cuda:0',
         overlap_size=0,
-        nth_layer=-1
+        nth_layer=-1,
+        use_fp16=False,
     ):
         self.device = device
+        self.use_fp16 = use_fp16 and device != 'cpu'
         self.overlap_size = overlap_size
         self.nth_layer = nth_layer
 
+        load_kwargs = {'cache_dir': cache_dir}
+        if self.use_fp16:
+            load_kwargs['torch_dtype'] = torch.float16
+
+        print(f'Loading VideoMAE encoder from {model_name} ...')
         self.image_processor = VideoMAEImageProcessor.from_pretrained(model_name, cache_dir=cache_dir)
-        self.model = VideoMAEModel.from_pretrained(model_name).to(self.device).eval()
-        
+        # Checkpoint includes a MAE decoder; VideoMAEModel loads encoder weights only.
+        self.model = VideoMAEModel.from_pretrained(model_name, **load_kwargs).to(self.device).eval()
+        print('VideoMAE encoder ready.')
+
     @torch.no_grad()
     def get_feats(self, video):
-        inputs = self.image_processor(images=video, return_tensors="pt").to(self.device)
-        
+        inputs = self.image_processor(images=video, return_tensors="pt")
+        inputs = _to_device(inputs, self.device, self.use_fp16)
+
         outputs = self.model(**inputs, output_hidden_states=True).hidden_states
-        
+
         outputs = outputs[self.nth_layer]
         outputs = outputs[:, 0]
-        
-        return outputs
+
+        return outputs.float()
 
 
-def get_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--anno_root', help='location of tsv files', required=True)
-    parser.add_argument('--video_root', help='location of tsv files', required=True)
-    parser.add_argument('--save_dir', help='where to save the output', required=True)
-    parser.add_argument('--model_name', help='ViT model name', default='MCG-NJU/videomae-large')
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--device', help='device to use', default='cpu')
-    parser.add_argument('--overlap_size', type=int, default=8)
-    parser.add_argument('--mode', nargs='+', type=str)
-    parser.add_argument('--nth_layer', type=int, default=-1)
-    parser.add_argument('--cache_dir', help='cache dir for model', default=None)
-    parser.add_argument(
-        '--force',
-        action='store_true',
-        help='Re-extract features even when output files already exist',
-    )
-    return parser
+class Args:
+    def __init__(self):
+        self.anno_root = '/kaggle/input/datasets/kowshiksaha006/isharakhobor-frames/preprocess'
+        self.video_root = '/kaggle/input/datasets/kowshiksaha006/isharakhobor-frames/frame_root'
+        self.device, self.use_fp16 = configure_gpu('cuda:0')
+        self.overlap_size = 8
+        self.batch_size = 8
+        self.nth_layer = -1
+        self.cache_dir = '/kaggle/working/hf_cache'
+        self.save_dir = '/kaggle/working/save/isharakhobor'
+        self.model_name = 'MCG-NJU/videomae-large'
+        self.force = False
 
 
 def build_mae_postfix(args, st):
@@ -87,21 +118,13 @@ def get_start_time(data_item, ds_name):
     return None
 
 
-def get_iterator(args, mode, item_save_dir):
+def get_iterator(args, mode, item_save_dir, reader):
     batch_size = args.batch_size
 
     data = np.load(os.path.join(args.anno_root, f'{mode}_info.npy'), allow_pickle=True).item()
-    ds_name = osp.split(args.anno_root)[-1]
+    ds_name = 'isharakhobor'
     num = len(data) if ds_name in ('Banglagov', 'BTVSL', 'isharakhobor') else len(data) - 1
 
-    reader = VideoMAEFeatureReader(
-        args.model_name, 
-        device=args.device, 
-        overlap_size=args.overlap_size, 
-        nth_layer=args.nth_layer,
-        cache_dir=args.cache_dir
-    )
-    
     def iterate():
         for i in range(num):
             st = get_start_time(data[i], ds_name)
@@ -113,74 +136,86 @@ def get_iterator(args, mode, item_save_dir):
                 continue
 
             fname = data[i]['folder']
-            
+
             if ds_name in ('Phoenix14T', 'CSL-Daily', 'Banglagov', 'BTVSL', 'isharakhobor'):
                 image_list = get_img_list(ds_name, args.video_root, fname)
-                
+
                 if len(image_list) < 16:
-                    len_diff = 16 - len(image_list)
                     image_list.extend([image_list[-1]] * (16 - len(image_list)))
-                image_list_chunks = sliding_window_for_list(image_list, window_size=16, overlap_size=args.overlap_size)
-                
+                image_list_chunks = sliding_window_for_list(
+                    image_list, window_size=16, overlap_size=args.overlap_size
+                )
+
                 videos = []
-                for image_list in image_list_chunks:
-                    videos.append([Image.open(image).convert('RGB') for image in image_list])
-                
+                for chunk in image_list_chunks:
+                    videos.append([Image.open(image).convert('RGB') for image in chunk])
+
                 video_feats = []
                 for j in range(0, len(videos), batch_size):
                     video_batch = videos[j:min(j + batch_size, len(videos))]
                     feats = reader.get_feats(video_batch).cpu().numpy()
                     video_feats.append(feats)
-                    
+
+                if args.device != 'cpu':
+                    torch.cuda.empty_cache()
                 yield np.concatenate(video_feats, axis=0), data[i]['fileid'], st, output_path
-            
+
             else:
                 if ds_name == 'How2Sign':
                     start_time, end_time = data[i]['original_info']['START_REALIGNED'], data[i]['original_info']['END_REALIGNED']
                     videos = read_video(fname, start_time=start_time, end_time=end_time)
-                    
+
                     if len(videos) > 0:
                         if len(videos) < 16:
-                            len_diff = 16 - len(videos)
                             videos.extend([videos[-1]] * (16 - len(videos)))
-                        
+
                         videos = sliding_window_for_list(videos, window_size=16, overlap_size=args.overlap_size)
-                        
+
                         video_feats = []
                         for j in range(0, len(videos), batch_size):
                             video_batch = videos[j:min(j + batch_size, len(videos))]
                             feats = reader.get_feats(video_batch).cpu().numpy()
                             video_feats.append(feats)
-                        
+
+                        if args.device != 'cpu':
+                            torch.cuda.empty_cache()
                         yield np.concatenate(video_feats, axis=0), data[i]['fileid'], st, output_path
-                    
+
                     else:
                         yield [], data[i]['fileid'], st, output_path
-    
+
     return iterate, num, data
 
+
 def main():
-    parser = get_parser()
-    args = parser.parse_args()
+    args = Args()
+    reader = VideoMAEFeatureReader(
+        args.model_name,
+        device=args.device,
+        overlap_size=args.overlap_size,
+        nth_layer=args.nth_layer,
+        cache_dir=args.cache_dir,
+        use_fp16=args.use_fp16,
+    )
 
     mode = ["dev", "test", "train"]
     try:
         for m in mode:
-            ds_name = osp.split(args.anno_root)[-1]
+            ds_name = 'isharakhobor'
             fname = f'mae_feat_{ds_name}'
             item_save_dir = osp.join(args.save_dir, fname, m)
             os.makedirs(item_save_dir, exist_ok=True)
             cleanup_stale_temp_files(item_save_dir)
-    
+
             if ds_name == 'How2Sign':
                 if m == 'dev': _m = 'val'
                 else: _m = m
             elif ds_name == 'NIASL2021':
-                if m == 'dev': _m = 'validation' 
+                if m == 'dev': _m = 'validation'
             else:
                 _m = m
 
-            generator, num, data = get_iterator(args, _m, item_save_dir)
+            generator, num, data = get_iterator(args, _m, item_save_dir, reader)
             output_paths = build_output_paths(
                 item_save_dir,
                 data,
--- scripts/vit_extract_feature.py
+++ scripts/vit_extract_feature.py
@@ -1,13 +1,11 @@
-import argparse
 import os
 import os.path as osp
-import glob
 import tqdm
 import torch
 import numpy as np
-import torch.nn.functional as F
 from PIL import Image
 from transformers import AutoImageProcessor, CLIPVisionModel
+from transformers.utils import logging as hf_logging
 
 import sys
 sys.path.append('./')
@@ -23,70 +21,88 @@ from utils.feature_extract_resume import (
     handle_keyboard_interrupt,
 )
 
+hf_logging.set_verbosity_error()
+os.environ.setdefault('TRANSFORMERS_NO_ADVISORY_WARNINGS', '1')
 
 _GLOBAL_SEED = 0
 np.random.seed(_GLOBAL_SEED)
 torch.manual_seed(_GLOBAL_SEED)
+torch.backends.cudnn.benchmark = True
 
+
+def configure_gpu(device='cuda:0'):
+    if not torch.cuda.is_available():
+        print('CUDA not available; falling back to CPU.')
+        return 'cpu', False
+
+    device_id = int(device.split(':')[-1]) if ':' in device else 0
+    name = torch.cuda.get_device_name(device_id)
+    major, minor = torch.cuda.get_device_capability(device_id)
+    use_fp16 = True
+    print(f'Using {name} on {device} (compute capability {major}.{minor}), fp16={use_fp16}')
+    return device, use_fp16
 
 
 class ViTFeatureReader(object):
     def __init__(
-        self, 
-        model_name='openai/clip-vit-large-patch14', 
+        self,
+        model_name='openai/clip-vit-large-patch14',
         cache_dir=None,
-        device='cuda:0', 
+        device='cuda:0',
         s2_mode='s2wrapping',
-        scales=[1, 2],
-        nth_layer=-1
+        scales=(1, 2),
+        nth_layer=-1,
+        use_fp16=False,
     ):
         self.s2_mode = s2_mode
         self.device = device
         self.scales = scales
         self.nth_layer = nth_layer
-        
-        self.model = CLIPVisionModel.from_pretrained(
-            model_name, output_hidden_states=True, cache_dir=cache_dir
-        ).to(device).eval()
-        
-        self.image_processor = AutoImageProcessor.from_pretrained(model_name)
+        self.use_fp16 = use_fp16 and device != 'cpu'
+
+        load_kwargs = {'output_hidden_states': True, 'cache_dir': cache_dir}
+        if self.use_fp16:
+            load_kwargs['torch_dtype'] = torch.float16
+
+        print(f'Loading CLIP vision encoder from {model_name} ...')
+        self.model = CLIPVisionModel.from_pretrained(model_name, **load_kwargs).to(device).eval()
+        self.image_processor = AutoImageProcessor.from_pretrained(model_name, cache_dir=cache_dir)
+        print('CLIP vision encoder ready.')
 
     @torch.no_grad()
     def forward_features(self, inputs):
+        if self.use_fp16 and inputs.dtype != torch.float16:
+            inputs = inputs.to(dtype=torch.float16)
         outputs = self.model(inputs).hidden_states
-        outputs = outputs[self.nth_layer]
-        return outputs
+        return outputs[self.nth_layer]
 
     @torch.no_grad()
     def get_feats(self, video):
-        inputs = self.image_processor(list(video), return_tensors="pt").to(self.device).pixel_values
+        inputs = self.image_processor(list(video), return_tensors="pt").pixel_values.to(self.device)
+        if self.use_fp16:
+            inputs = inputs.to(dtype=torch.float16)
         if self.s2_mode == "s2wrapping":
-            outputs = multiscale_forward(self.forward_features, inputs, scales=self.scales, num_prefix_token=1)
+            outputs = multiscale_forward(
+                self.forward_features, inputs, scales=self.scales, num_prefix_token=1
+            )
         else:
             outputs = self.forward_features(inputs)
-        return outputs[:, 0]
+        return outputs[:, 0].float()
 
 
-def get_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--anno_root', help='location of tsv files', required=True)
-    parser.add_argument('--video_root', help='location of tsv files', required=True)
-    parser.add_argument('--device', help='device to use', default='cuda:0')
-    parser.add_argument('--s2_mode', default='')
-    parser.add_argument('--scales', nargs='+', type=int, help='List of scales', default=[])
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--nth_layer', type=int, default=-1)
-    parser.add_argument('--cache_dir', help='cache dir for model', default=None)
-    
-    parser.add_argument('--save_dir', help='where to save the output', required=True)
-    parser.add_argument('--model_name', help='ViT model name', default='openai/clip-vit-large-patch14')
-    parser.add_argument(
-        '--force',
-        action='store_true',
-        help='Re-extract features even when output files already exist',
-    )
-
-    return parser
+class Args:
+    def __init__(self):
+        self.anno_root = '/kaggle/input/datasets/kowshiksaha006/isharakhobor-frames/preprocess'
+        self.video_root = '/kaggle/input/datasets/kowshiksaha006/isharakhobor-frames/frame_root'
+        self.device, self.use_fp16 = configure_gpu('cuda:0')
+        self.s2_mode = 's2wrapping'
+        self.scales = [1, 2]
+        self.batch_size = 8
+        self.nth_layer = -1
+        self.cache_dir = '/kaggle/working/hf_cache'
+        self.save_dir = '/kaggle/working/save/isharakhobor'
+        self.model_name = 'openai/clip-vit-large-patch14'
+        self.force = False
 
 
 def build_vit_postfix(args, st):
@@ -106,21 +122,13 @@ def get_start_time(data_item, ds_name):
     return None
 
 
-def get_iterator(args, mode, item_save_dir):
+def get_iterator(args, mode, item_save_dir, reader):
     batch_size = args.batch_size
-    
+
     data = np.load(os.path.join(args.anno_root, f'{mode}_info.npy'), allow_pickle=True).item()
-    ds_name = osp.split(args.anno_root)[-1]
+    ds_name = 'isharakhobor'
     num = len(data) if ds_name in ('Banglagov', 'BTVSL', 'isharakhobor') else len(data) - 1
-    reader = ViTFeatureReader(
-        args.model_name, 
-        device=args.device, 
-        s2_mode=args.s2_mode, 
-        scales=args.scales,
-        nth_layer=args.nth_layer,
-        cache_dir=args.cache_dir
-    )
-    
+
     def iterate():
         for i in range(num):
             st = get_start_time(data[i], ds_name)
@@ -132,60 +140,72 @@ def get_iterator(args, mode, item_save_dir):
                 continue
 
             fname = data[i]['folder']
-            
+
             if ds_name in ('Phoenix14T', 'CSL-Daily', 'Banglagov', 'BTVSL', 'isharakhobor'):
                 image_list = get_img_list(ds_name, args.video_root, fname)
                 videos = [Image.open(image).convert('RGB') for image in image_list]
-                
+
                 video_feats = []
                 for j in range(0, len(videos), batch_size):
                     video_batch = videos[j:min(j + batch_size, len(videos))]
                     feats = reader.get_feats(video_batch).cpu().numpy()
                     video_feats.append(feats)
-                
+
+                if args.device != 'cpu':
+                    torch.cuda.empty_cache()
                 yield np.concatenate(video_feats, axis=0), data[i]['fileid'], st, output_path
-            
+
             else:
                 if ds_name == 'How2Sign':
                     start_time, end_time = data[i]['original_info']['START_REALIGNED'], data[i]['original_info']['END_REALIGNED']
                     videos = read_video(fname, start_time=start_time, end_time=end_time)
-            
+
                 if len(videos) > 0:
                     video_feats = []
                     for j in range(0, len(videos), batch_size):
                         video_batch = videos[j:min(j + batch_size, len(videos))]
                         feats = reader.get_feats(video_batch).cpu().numpy()
                         video_feats.append(feats)
+                    if args.device != 'cpu':
+                        torch.cuda.empty_cache()
                     yield np.concatenate(video_feats, axis=0), data[i]['fileid'], st, output_path
                 else:
                     yield [], data[i]['fileid'], st, output_path
-    
+
     return iterate, num, data
 
 
 def main():
-    parser = get_parser()
-    args = parser.parse_args()
-    mode = ["dev", "test", "train"]
+    args = Args()
+    reader = ViTFeatureReader(
+        args.model_name,
+        device=args.device,
+        s2_mode=args.s2_mode,
+        scales=args.scales,
+        nth_layer=args.nth_layer,
+        cache_dir=args.cache_dir,
+        use_fp16=args.use_fp16,
+    )
 
+    mode = ["dev", "test", "train"]
     try:
         for m in mode:
-            ds_name = osp.split(args.anno_root)[-1]
+            ds_name = 'isharakhobor'
             _model_name = os.path.split(args.model_name)[-1]
             fname = f'{_model_name}_feat_{ds_name}'
             item_save_dir = osp.join(args.save_dir, fname, m)
             os.makedirs(item_save_dir, exist_ok=True)
             cleanup_stale_temp_files(item_save_dir)
-        
+
             if ds_name == 'How2Sign':
                 if m == 'dev': _m = 'val'
                 else: _m = m
             elif ds_name == 'NIASL2021':
-                if m == 'dev': _m = 'validation' 
+                if m == 'dev': _m = 'validation'
             else:
                 _m = m
 
-            generator, num, data = get_iterator(args, _m, item_save_dir)
+            generator, num, data = get_iterator(args, _m, item_save_dir, reader)
             output_paths = build_output_paths(
                 item_save_dir,
                 data,
