19aebc03dba4ab3f733a13e88ea8272e755c694f
Kowshik Saha <kowshik.saha.ks.ks@gmail.com>
2026-07-19 22:07:46 +0600

    Added cmd line arg for passing npy file path.

--- utils/check_npy_structure.py
+++ utils/check_npy_structure.py
@@ -1,8 +1,26 @@
 import numpy as np
+import argparse
 
-data = np.load("preprocess/Phoenix14T/train_info.npy", allow_pickle=True)
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Check the structure of the npy file"
+    )
+    parser.add_argument(
+        '--path', required=True,
+        help='Path to the npy file'
+    )
+    return parser
 
-item = data.item()[0]
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
 
-for key, value in item.items():
-    print(f"{key} : {value}")
+    data = np.load(args.path, allow_pickle=True)
+
+    item = data.item()[0]
+
+    for key, value in item.items():
+        print(f"{key} : {value}")
+
+if __name__ == "__main__":
+    main()
